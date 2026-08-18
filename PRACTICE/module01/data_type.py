6ddc7a4c66bcdbd5f52f1be8a9dd7dd25a93835c
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-18 23:03:16 +0530

    data type in python

---
+++ PRACTICE/module01/data_type.py
@@ -0,0 +1,29 @@
+#Data Type :
+#IT has Devided some part .
+#Data type is identity of value .
+
+# int
+# float
+# complex
+# bool
+# str
+
+# collection :
+# list
+# tuple
+# set
+# dict
+
+# Special :
+# None
+
+age = 21              # int
+print(age)
+price = 99.5          # float
+print(price)
+name = "Python"       # str
+print(name)
+is_valid = True       # bool
+print(is_valid)
+numbers = [1, 2, 3]   # list
+print(numbers)
