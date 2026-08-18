7b9436bbee3b26bf17ab24e4356c6bf874911e9d
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-18 21:30:18 +0530

    variable of python

---
+++ PRACTICE/module01/variable.py
@@ -0,0 +1,14 @@
+# What is variable ?
+# -->A variable is a name given to a place in memory where we can store a value.
+#EX -name ="jyoti";
+
+name = "Akash"
+print(name)
+age = 21
+print(age)
+salary = 25000
+print(salary)
+
+print(type(age))
+print(type(name))
+# ptyhon auto matically detect type .
