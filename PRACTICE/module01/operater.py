d4209addb7ee80c232ca6a14766721865b1e1378
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-18 23:42:51 +0530

    arethomatic operater

---
+++ PRACTICE/module01/operater.py
@@ -0,0 +1,38 @@
+# arethomatic operater :
+# + ,  - ,  * ,  / ,  // , % , ** 
+
+a = 20
+b = 5
+
+# addition
+print(a+b)
+
+# Substaction :
+print(a-b)
+
+# multiplication :
+print(a*b)
+
+# power :
+print(a**b)
+
+# reminder :c
+print(a%b)
+
+# devision :
+print(a / b)
+
+# floore devision :
+print(a//b)
+
+
+
+# COMPARESION OPERATER :
+# == 
+# !=
+# >
+# <
+# >=
+# <=
+
+
