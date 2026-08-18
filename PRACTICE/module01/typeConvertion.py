679c4c868a4d48b5c8a983d19aebbd308a090638
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-18 23:29:15 +0530

    Type convertion

---
+++ PRACTICE/module01/typeConvertion.py
@@ -0,0 +1,12 @@
+# it mean coner tion of data type .
+
+
+j = 10
+print (type(j))
+k = 10.1
+print(type(k))
+l = True
+print(type(l))
+
+print(float(10)+10)#It has covert the int value to float value .
+print(int(k))#it has convert the float valu to integer value . 
