9c47459f1f129bbec5bd4d47a57b44271eb495d0
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-27 20:18:49 +0530

    function chenges don

--- PRACTICE/module01/function.py
+++ PRACTICE/module01/function.py
@@ -8,12 +8,12 @@
 # : → starts the function body .
 
 
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
 
-result = add(10, 20)
-print(result)
+# result = add(10, 20)
+# print(result)
 
 # 1.PARAMETER :
 # -->parameter are the variable writen inside the function defination ,
@@ -27,12 +27,12 @@ print(result)
 
 # 3.RETURN :
 # -->Return send a value back from function .
-def Square(num):
-    return num * num
+# def Square(num):
+#     return num * num
 
 
-result = Square(5)
-print(result)
+# result = Square(5)
+# print(result)
 
 
 # RETURN vs PRINT
@@ -44,9 +44,62 @@ print(result)
 # -->A parameter can have default value .
 # -->Default arguments are used when the caller doesnot provide that argument .
 # -->Ex -
-def greet(name, message="hello"):
-    print(message, name)
-    greet("jyoti")
+# def greet(name, message="Hello"):
+#     print(message, name)
+
+# greet("Rahul")
+
+# KEYWORD ARGUMENT :
+# -->Insert the passing value according to position , we can specyfy the parameter name  .
+# def student(name, age):
+#     print(name)
+#     print(age)
+# #student("jyoti",32)#Positional argument .
+
+# student(age=21, name="Rahul")#key word argument .
 
 
- 
+# *args :
+# -->Args is use to when we dont know how many positional argument will be passed .
+# -->*args allwos to function to accept multiple positional argument .
+# def add(*numbers):
+#     total = 0
+
+#     for num in numbers:
+#         total += num
+
+#     return total
+
+
+# print(add(10, 20))
+# print(add(10, 20, 30))
+# print(add(10, 20, 30, 40))
+
+
+# # in side thee function , number behave like tuple .
+# def show(*args):
+#     print(args)
+
+
+# show(10, 20, 30)
+
+
+# **kwaargs :
+
+# --> It allow to function accept multiple key word argument .
+# -->Inside the function details behave like dicitionary .
+
+
+# def student(**details):
+#     print(details)
+
+
+# student(name="jyoti", age=21, city="cuttack")
+
+
+def student(**details):
+    for key, value in details.items():
+        print(key, ":", value)
+
+
+student(name="Rahul", age=21, course="Python")
