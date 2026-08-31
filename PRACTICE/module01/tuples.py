207a2cdea3406bceaaea738c167d9b30b6792594
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-31 22:17:58 +0530

    tuole

--- PRACTICE/module01/tuples.py
+++ PRACTICE/module01/tuples.py
@@ -3,7 +3,6 @@
 # -->it has mentain order of an element .
 # -->It has immmutable -it cannot be change after creation .
 # -->Allows Duplicates -Same value appeair multiple times .
-#  
 
 data = (10, 20, 30, 40, 50)
 print(data)
@@ -25,3 +24,20 @@ print(data)
 data = (100, 100, 20, 20, 30, 40)
 print(data)
 
+# Tuple Method :
+# --> tuppls have fewer method because they cannpt modify .
+
+# count() : count how many a value appears .
+
+
+data = (10, 20, 20, 30)
+
+print(data.count(30))
+print(data.count(10))
+print(data.count(20))
+
+# index() : return the index of value .
+
+data = (10, 20, 30)
+
+print(data.index(20))
