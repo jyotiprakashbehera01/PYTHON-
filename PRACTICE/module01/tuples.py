6f910f33e2d4bc3eabb421023c65f86d2fb5d2c4
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-08-31 09:04:26 +0530

    tuple in python

---
+++ PRACTICE/module01/tuples.py
@@ -0,0 +1,27 @@
+# TUPLES :
+# -->Tuples is a collection of multiple values stored together .
+# -->it has mentain order of an element .
+# -->It has immmutable -it cannot be change after creation .
+# -->Allows Duplicates -Same value appeair multiple times .
+#  
+
+data = (10, 20, 30, 40, 50)
+print(data)
+
+# Access tuple element :
+
+print(data[0])
+print(data[-1])
+
+# Immutable :
+# data[0] = 100
+
+data = (100, 200, 300)
+
+print(data)
+
+# tuple allow auplicate :
+
+data = (100, 100, 20, 20, 30, 40)
+print(data)
+
