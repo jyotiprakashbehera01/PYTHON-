24addaff361842b77160e9de168ddbe10f74b249
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-09-21 15:57:38 +0530

    collect to student information

--- PROBLEM/mode1.py
+++ PROBLEM/mode1.py
@@ -1,7 +1,7 @@
 # Problem 1: Welcome to Python 
 # You have been asked to create a small program that welcomes a new user. When the program starts, 
 # it should ask the user for their name and then display a friendly welcome message using their name.
-# 
+
 
 name = input("please enter your name: ")
 
---
+++ PROBLEM/mode2.py
@@ -0,0 +1,16 @@
+# Problem 2: Student Registration 
+# A college is collecting basic information from newly admitted students.
+# Write a program that asks the student for their name, age, and course of study. 
+# After collecting the information, display all the details in a neat and readable format.
+
+# Ask for user details :
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+course = input("Enter your course of study: ")
+
+# Disply the collected information
+print("\n-------Student Information--------")
+print(f"Name   :{name}")
+print(f"Age    :{age}")
+print(f"Course :{course}")
+print("------------------------------------")
