edf7200c4971c26b2d9483e462dfe87214172e82
jyotiprakashbehera01 <bjyotiprakash04@gmail.com>
2026-09-24 20:56:32 +0530

    some update

---
+++ copy/cl3.py
@@ -0,0 +1,63 @@
+# Conditional Statements
+
+# if-elif-else -> When the multiple conditions, that are in range
+
+# marks = float(input("Enter the marks: "))
+# if marks >= 90:
+#     print("Grade A")
+# elif marks >= 80 and marks < 90:
+#     print("Grade B")
+# elif marks >= 70 and marks < 80:
+#     print("Grade C")
+# else:
+#     print("Grade D")
+
+# switch-case -> When you want to compare a variable against multiple cases
+
+# day = int(input("Enter the day number: "))
+# match day:
+#     case 1:
+#         print("Monday")
+#     case 2:
+#         print("Tuesday")
+#     case 3:
+#         print("Wednesday")
+#     case 4:
+#         print("Thursday")
+#     case 5:
+#         print("Friday")
+#     case 6:
+#         print("Saturday")
+#     case 7:
+#         print("Sunday")
+#     case _: # for default cases
+#         print("Enter a valid day number!")
+
+# Iterative Statements
+
+# for loop -> When you know the number of iterations
+
+# syntax
+# for iterator in range:
+#     business logic
+
+# for i in range(5):
+#     print(i)
+
+# ls = [1, 2, 3, 4, 5]
+# for i in ls:
+#     if i % 2 == 0:
+#         print(f"{i} is even")
+#     else:
+#         print(f"{i} is odd")
+
+# while loop -> Works on condition
+
+# syntax
+# while condition:
+#     business logic
+
+# count = 0
+# while count <= 5:
+#     print(count, end=" ")
+#     count += 1 # incrementation (count = count + 1)
